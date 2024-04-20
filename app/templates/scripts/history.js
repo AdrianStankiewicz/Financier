6e13f32969155c961467882d9f98d3681b75798d
Adrian Stankiewicz <AdrianStankiewiczIT@gmail.com>
2024-04-20 09:27:47 +0000

    created history template

--- app/templates/scripts/history.js
+++
@@ -1,4 +0,0 @@
-function updateFormAction() {
-    var selectedCurrency = document.getElementById("currency_id").value;
-    document.getElementById("currencyForm").setAttribute("hx-get", "/currency_history_check/" + selectedCurrency);
-}
